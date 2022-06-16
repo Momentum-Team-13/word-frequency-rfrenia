9ca36acff6af739a3953e01076d652d778208db6
rfrenia <ryanfrenia@gmail.com>
2022-06-16 11:21:27 -0400

    counter is working

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,7 @@
+#from operator import truediv
+#from optparse import Values
+
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -13,15 +17,30 @@ def print_word_freq(file):
         no_punct = read_file.replace(",", "").replace(".", "").replace("?", "")
         #print(no_punct)
         split_word = no_punct.lower()#.replace(" ", "")
-        new_list = split_word.split(' ')
+        new_list = split_word.split()#.replace(STOP_WORDS, "")
         #print(new_list)
 
-    def get_numbers(s, e, i):
-        return list(range(s, e, i))
-    start, end, intval = 1, 662, 1
-    zip_list = zip(get_numbers(start, end, intval), new_list)
-    print(zip_list)
-    print(dict(zip_list))
+    make_dictionary = dict.fromkeys(new_list, 0)
+    for word in new_list:
+        make_dictionary[word] += 1
+    print(make_dictionary)
+
+
+
+    # def get_numbers(s, e, i):
+    #     return list(range(s, e, i))
+    # start, end, intval = 1, 662, 1
+    # zip_list = zip(get_numbers(start, end, intval), new_list)
+    # #print(zip_list)
+    # new_dict = dict(zip_list)
+    # print(new_dict)
+
+
+    # for keys in new_dict:
+    #     if new_dict[Values] == new_dict[Values]:
+    #         print(keys)
+
+
 
 #        print(f' "we" appears {new_list.count("we")} time(s)')
 
