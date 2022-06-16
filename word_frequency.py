c1b2633ce3762b6dd56f1868b33117376e8a2bd9
rfrenia <ryanfrenia@gmail.com>
2022-06-15 21:49:56 -0400

    start of word frequency

--- word_frequency.py
+++ word_frequency.py
@@ -6,9 +6,24 @@ STOP_WORDS = [
 
 
 def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    pass
+    """Read in `file` and print out the frequency of words in that file. Your code goes here"""
+    print(f'Your file is: {file}')
+    with open(file) as open_file:
+        read_file = open_file.read()
+        no_punct = read_file.replace(",", "").replace(".", "").replace("?", "")
+        #print(no_punct)
+        split_word = no_punct.lower()#.replace(" ", "")
+        new_list = split_word.split(' ')
+        #print(new_list)
 
+    def get_numbers(s, e, i):
+        return list(range(s, e, i))
+    start, end, intval = 1, 662, 1
+    zip_list = zip(get_numbers(start, end, intval), new_list)
+    print(zip_list)
+    print(dict(zip_list))
+
+#        print(f' "we" appears {new_list.count("we")} time(s)')
 
 if __name__ == "__main__":
     import argparse
